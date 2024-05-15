129192f2c3eb53d757c7aa28e4630803301c5d0e
DemarionM <37543775+DemarionM@users.noreply.github.com>
2024-05-15 09:29:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
-const startButton = document.querySelector('#startButton');
+const startButton = document.querySelector('#start');
 // TODO: Add the missing query selectors:
 const score = document.querySelector('#score'); 
 const timerDisplay = document.querySelector('#timer');
@@ -259,7 +259,7 @@ function setDuration(duration) {
 * timer using clearInterval. Returns "game stopped".
 *
 */
-function gameOver() {
+function stopGame() {
   clearInterval(timer);
   return "game stopped";
   }
@@ -279,7 +279,9 @@ function startGame(){
    return "game started";
 }
 
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", function() {
+    startGame();
+});
 
 
 setEventListeners();
